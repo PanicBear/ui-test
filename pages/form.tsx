754ec73e41e1c03af1449bfe8b77982307c20454
ClarXo <imp_rove@naver.com>
2022-09-04 23:29:02 +0900

    add error message

--- pages/form.tsx
+++ pages/form.tsx
@@ -27,17 +27,21 @@ const Form:NextPage = () => {
   const submitForm = (data:any) => {
     console.log(data);
   }; 
-  
+
   return <div>
-    <form onSubmit={handleSubmit(submitForm)}> //*
+    <form onSubmit={handleSubmit(submitForm)}> 
       <label htmlFor="email">이메일</label>
       <input type="email" {...register('email')}/>
+	    <span>{errors.email && '이메일 형식이 맞지 않습니다.'}</span>
       <label htmlFor="name">이름</label>
       <input type="text" {...register('name')}/>
+      <span>{errors.name && '이름 형식이 맞지 않습니다.'}</span>
       <label htmlFor="pw">비밀번호</label>
       <input type="text" {...register('pw')}/>
+      <span>{errors.pw && '비밀번호 형식이 맞지 않습니다.'}</span>
       <label htmlFor="checkPw">비밀번호 확인</label>
       <input type="text" {...register('checkPw')}/>
+      <span>{errors.checkPw && '비밀번호가 맞지 않습니다.'}</span>
       <button type="submit">회원가입</button>
     </form>
   </div>
